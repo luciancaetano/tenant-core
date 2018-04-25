710d7367e3d3e1e40398b2cce81b27cabe7b4ecc
Lucian Caetano <lucian.santos.caetano@gmail.com>
2018-04-24 21:38:43 -0300

    test

--- tenantcore-admin-js/src/app/app.routing.js
+++ tenantcore-admin-js/src/app/app.routing.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import * as Modules from './modules';
 import { Locale } from '@luciancaetano/i18n';
 
-export default {
+interface IRoutingEntry{
+    exact: boolean;
+    component: ReactNode;
+    title: ReactNode;
+}
+export interface IRoutingType{
+    [key:string]: IRoutingEntry;
+}
+
+const routes:IRoutingType = {
     '/': {
         exact: true,
         component: Modules.HomeView,
         title: <Locale entry={'dashboard'}/>
     }
-};
+}
+
+export default routes;
